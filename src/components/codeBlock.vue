e9d531e80e9f9bd1c920f3a847641652f514ac8e
loickengit <1220864925@qq.com>
2020-02-12 20:47:40 +0800

    embbed ace editor, add another route for interview coding

---
+++ src/components/codeBlock.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <h1>Socket.io is Here</h1>
+    <editor id="editor" v-model="content" ref="editor" @init="editorInit" lang="html"
+            theme="chrome" width="700" height="100"></editor>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'coding',
+    components: {
+      editor: require('vue2-ace-editor'),
+    },
+    methods: {
+      editorInit: function () {
+        require('brace/ext/language_tools') //language extension prerequsite...
+        require('brace/mode/html')
+        require('brace/mode/python')    //language
+        require('brace/mode/less')
+        require('brace/theme/chrome')
+        require('brace/snippets/python') //snippet
+
+      },
+    },
+    mounted () {
+      let editor = this.$refs.editor.editor
+
+      // editor.setValue("the new text here");
+      editor.session.setValue("the new text here");
+    }
+  }
+
+</script>
+
+<style scoped>
+  #editor {
+    position: relative;
+    top: 10%;
+    right: 10%;
+    bottom: 0;
+    left: 0;
+  }
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Welcome from '@/components/Welcome'
 import Main from '@/components/Main'
+import CodeBlock from '@/components/codeBlock'
 
 Vue.use(Router)
 
@@ -20,21 +21,29 @@ let router = new Router({
       meta: {
         requireAuth:true
       }
+    },
+    {
+      path: '/coding',
+      name: 'coding',
+      component: CodeBlock,
+      meta: {
+        requireAuth: true
+      }
     }
   ]
 })
 
 export default router
 
-router.beforeEach((to, from, next) => {
-  let islogin = localStorage.getItem("MIState");
-  islogin = Boolean(Number(islogin));
-  if (to.meta.requireAuth) {
-    if (islogin) next();
-    else next("/");
-  }else{
-    console.log(islogin)
-    if (islogin) next("/main");
-    else next();
-  }
-})
+// router.beforeEach((to, from, next) => {
+//   let islogin = localStorage.getItem("MIState");
+//   islogin = Boolean(Number(islogin));
+//   if (to.meta.requireAuth) {
+//     if (islogin) next();
+//     else next("/");
+//   }else{
+//     console.log(islogin)
+//     if (islogin) next("/main");
+//     else next();
+//   }
+// })
